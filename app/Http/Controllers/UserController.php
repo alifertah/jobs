625dbb562ab29ff2a5c36cf860c872aca25cc71d
alifertah <alifertah42@gmail.com>
2024-02-07 16:11:31 +0100

    fix : add role to column to user table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,16 +7,18 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    /**
+     * try to authenticate, in fail it stays in the same page
+     */
     public function login(Request $r){
         $credentials = $r->only('email', 'password');
         $remember = $r->filled("remember");
 
         if(Auth::attempt($credentials, $remember)){
-            return redirect()->intended('/');
+            return redirect("/dashboard");
         }
         return redirect()->back()->withErrors([
             'login' => 'These credentials do not match our records.',
         ]);
-
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
 Route::post("/subscribe", [NewsLetterController::class, 'subscribe'])->name("subscribe");
 Route::post("/register", [RegisterControl::class, 'registerUser'])->name("register");
 Route::post("/login", [UserController::class, 'login'])->name("login");
